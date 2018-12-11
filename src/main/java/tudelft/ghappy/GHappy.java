abf6170b03b820770c2f2285d65da277fd28c711
magrasjcy <jclaudemag@att.net>
2018-12-11 00:40:35 +0000

    homework

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -4,15 +4,17 @@ public class GHappy {
 
     public boolean gHappy(String str) {
         assert str!=null;
+        if (str.length()==0) return false;
+        boolean result=false;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
-                return false;
+                if (i >= 1 && str.charAt(i-1) == 'g') { return true; }
+                if (i+1 < str.length() && str.charAt(i+1) == 'g') { return true;}
+
             }
         }
 
-        return true;
+        return result;
 
     }
 }
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,52 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void unhappystring() {
+        boolean result = new GHappy().gHappy("xxxyzz");
+        Assertions.assertEquals(false,result);
+    }
+    @Test
+    public void emptystring() {
+        boolean result = new GHappy().gHappy("");
+        Assertions.assertEquals(false,result);
+    }
+    @Test
+    public void happystart() {
+        boolean result = new GHappy().gHappy("ggXsdlifsd");
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+    public void slightlyhappystart() {
+        boolean result = new GHappy().gHappy("gslijeojfd");
+        Assertions.assertEquals(false,result);
+    }
+    @Test
+    public void happyslightly() {
+        boolean result = new GHappy().gHappy("hgtyttr");
+        Assertions.assertEquals(false,result);
+    }
+    @Test
+    public void hiddenhappystart() {
+        boolean result = new GHappy().gHappy("mggXsdlifsd");
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+    public void happyend() {
+        boolean result = new GHappy().gHappy("00Xsdlifgg");
+        Assertions.assertEquals(true,result);
+    }
+    @Test
+    public void slightlyhappyend() {
+        boolean result = new GHappy().gHappy("Xsdlifsdg");
+        Assertions.assertEquals(false,result);
+    }
+    @Test
+    public void hiddenhappyend() {
+        boolean result = new GHappy().gHappy("Xsdlifsggd");
+        Assertions.assertEquals(true,result);
+    }
 }
